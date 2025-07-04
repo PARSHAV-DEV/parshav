9593dc2c865b924137142d61f15ebdece31ce1a5
parshav <parshavgundigara0123@gmail.com>
2025-07-04 20:21:55 +0530

    add

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import emailjs from 'emailjs-com'
+import emailjs from '@emailjs/browser'
 
 // Dummy data for certificates; replace URLs as needed
 const certificates = [
